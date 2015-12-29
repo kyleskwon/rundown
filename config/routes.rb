0fc1f12da7ba81692fbdd1edce019c4cae5ab12a
Kyle Kwon <kyleskwon@gmail.com>
2015-12-28 21:32:05 -0800

    Update routes and inflections, add events controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :registered_applications
   devise_for :users
 
+  namespace :api, defaults: { format: :json } do
+     match '/events', to: 'events#preflight', via: [:options]
+     resources :events, only: [:create]
+  end
+
   get 'home/index'
   root to: "home#index"
 end
